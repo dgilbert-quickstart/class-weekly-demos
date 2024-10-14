1f3339cfbac1cced60d70bc619108a73c5bf6a35
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-14 19:48:49 +0100

    update demo js intro dom app 1

---
+++ week4/demo-intro-js-dom-app-1/js/index.js
@@ -0,0 +1,14 @@
+
+function func2()
+{
+    let msg1 = "Js intro "
+    const msg2 = "es1..es5+";
+
+    const msg = msg1 + msg2;
+
+    //alert(msg1 + msg2);
+    alert(msg)
+
+    console.log(msg);
+}
+
