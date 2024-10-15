66adc343efc878a45e0874ec7741ab7e2d5ef6e4
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-15 20:39:33 +0100

    update page2, page2.js

---
+++ week4/demo-intro-js-dom-app-1/js/page2.js
@@ -0,0 +1,18 @@
+function page_load()
+{
+  const msg  = "## page load ..."
+  console.log(msg)
+}
+
+function demo_func1()
+{
+    const msg  = "## demo_func1 ..."
+    console.log(msg)
+}
+
+function demo_dom_1()
+{
+    const msg  = "## demo_dom_1 ..."
+    console.log(msg)
+}
+
