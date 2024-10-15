246bd55b82a0cefbce1a1ad45d2a711544acfb68
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-15 18:55:31 +0100

    update page1, page1.js

--- week4/demo-intro-js-dom-app-1/js/page1.js
+++ week4/demo-intro-js-dom-app-1/js/page1.js
@@ -17,3 +17,42 @@ function func3()
 
 }
 
+function dom_ex5() {
+
+    const msg = "### dom_ex5 called";
+    console.log(msg)
+
+    //select 1 or more elements using document - DOM library 
+    const outputDiv = document.getElementById("output");
+
+    /*
+    if (outputDiv != null){
+        //true 
+    }
+    else if (outputDiv != undefined) {
+       //true 
+    }
+    else 
+    {
+      //default 
+    }
+    */
+
+    //-- example of DOM - element, selection, element update, element style updaet etc..
+    //--- ie: SPA: react, angularm, vue , bootstrap, css
+
+    if (outputDiv != null | outputDiv != undefined){
+        //true 
+        //outputDiv.innerText = "<b>## update div element example. </b>";
+        outputDiv.innerHTML = "<b>## update div element example. </b>";
+        //update div style 
+    }
+    else
+    {
+        //false 
+        console.log("## div output not found")
+    }
+
+}
+
+
