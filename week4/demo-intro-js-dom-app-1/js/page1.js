02d306ee2a02b92fcba0bb2cb19df8fbf57b83a7
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-15 17:32:17 +0100

    update page1, page1.js

--- week4/demo-intro-js-dom-app-1/js/page1.js
+++ week4/demo-intro-js-dom-app-1/js/page1.js
@@ -5,3 +5,15 @@ function page_load()
     console.log(msg);
 }
 
+function func3()
+{
+    //default let 
+    //msg = "function 3 called";
+
+    const msg = "function 3 called";
+
+    //alert(msg)
+    console.log(msg)
+
+}
+
