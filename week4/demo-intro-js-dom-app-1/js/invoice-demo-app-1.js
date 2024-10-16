c82373c3d9a369a826c90cecff8b39d0a9917d70
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-16 18:37:21 +0100

    update invoice-demo-app-1

---
+++ week4/demo-intro-js-dom-app-1/js/invoice-demo-app-1.js
@@ -0,0 +1,13 @@
+function page_load()
+{
+    const msg = "## invoice-demo-app-1 - page load"
+    console.log(msg)
+}
+
+function calc_inv_ex1()
+{    
+    const msg = "## calc_inv_ex1"
+    console.log(msg)
+}
+
+
