c0d171cb8799a4b1fa899d63c0c72a0388fcc829
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-16 18:46:30 +0100

    update invoice-demo-app-1

--- week4/demo-intro-js-dom-app-1/js/invoice-demo-app-1.js
+++ week4/demo-intro-js-dom-app-1/js/invoice-demo-app-1.js
@@ -6,8 +6,24 @@ function page_load()
 
 function calc_inv_ex1()
 {    
-    const msg = "## calc_inv_ex1"
+    let msg = "## calc_inv_ex1"; //data type string 
     console.log(msg)
+
+    const _outputDiv = document.getElementById("output"); //data type - object
+
+    if(_outputDiv == null | _outputDiv == undefined)
+    {
+        msg = "## output div is not found"
+        console.log(msg)
+        
+        //exit function, does not continue code after if statement
+        return false;
+    }
+
+    //continue code after if statment 
+    _outputDiv.innerText = msg;
+
+    
 }
 
 
