d048c0677c3045d2aadec7d9db3c1f6e3ecccc95
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-17 17:39:45 +0100

    update contact-us-app-1

--- week4/demo-intro-js-dom-app-1/js/contact-us-app-1.js
+++ week4/demo-intro-js-dom-app-1/js/contact-us-app-1.js
@@ -1,18 +1,30 @@
 function page_load()
 {
-    msg = "## contact-us - page load";
+    const msg = "## contact-us - page load";
     console.log(msg)
 }
 
 function form_submit()
 {
-    msg = "## contact-us - form_submit";
+    let msg = "## contact-us - form_submit";
     console.log(msg)
+
+    const _outputDev = document.getElementById("output");
+
+    if(_outputDev == null | _outputDev == undefined)
+    {
+        msg = "## output div not found."
+        console.log(msg);
+        return false;
+    }
+
+    _outputDev.innerText = msg;
+
 }
 
 function form_reset()
 {
-    msg = "## contact-us - form-rest";
+    let msg = "## contact-us - form-rest";
     console.log(msg)
 }
 
