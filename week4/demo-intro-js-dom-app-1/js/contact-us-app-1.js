0ddef9ffd03a96ce6c90a6367bf4a3594193c798
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-17 17:29:25 +0100

    update contact-us-app-1

--- week4/demo-intro-js-dom-app-1/js/contact-us-app-1.js
+++ week4/demo-intro-js-dom-app-1/js/contact-us-app-1.js
@@ -1,5 +1,18 @@
 function page_load()
 {
-    msg = "## contact-us page load";
+    msg = "## contact-us - page load";
     console.log(msg)
 }
+
+function form_submit()
+{
+    msg = "## contact-us - form_submit";
+    console.log(msg)
+}
+
+function form_reset()
+{
+    msg = "## contact-us - form-rest";
+    console.log(msg)
+}
+
