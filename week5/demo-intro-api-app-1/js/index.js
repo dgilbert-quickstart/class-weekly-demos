1ca631949b6c6f7aa4d90346a52d4fbd5803cc66
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-21 21:36:09 +0100

    update Demo Api Intro App 1

--- week5/demo-intro-api-app-1/js/index.js
+++ week5/demo-intro-api-app-1/js/index.js
@@ -50,3 +50,39 @@ function js_obj_ex1()
 
 }
 
+function js_obj_ex2()
+{
+    const msg = "## js object example 2" // string 
+    console.log(msg)
+
+    const num1 = 5      //integer - non decimal 
+    const num2 = 5.1    //float - decimal point 
+
+    const obj1 = {} //empty object data type - also a JSON object 
+    
+    const obj2 = {name:"name1", price:1.0} //object data tyep - also a JSON object - key/value 
+
+    const obj3 = [{name:"name1", price:1.0}] //ayyay/list of object data tyep - also a JSON object - key/value 
+
+    //const _outputDiv = document.getElementById("output")
+    const _outputDiv = document.querySelector("#output") //retrieve element by id 
+    //const _outputDiv = document.querySelector(".output") //using css selector syntax
+
+    if(_outputDiv == null | _outputDiv == undefined){
+
+        msg = "## output dic not found"
+        console.log(msg)
+        return false;
+    }
+
+    //-- console.log json data 
+    console.log(`### num2: ${num2}, obj2: ${JSON.stringify(obj2)}`)
+
+    //_outputDiv.innerText = `num2: ${num2}, ${JSON.stringify(obj2)}`;
+    //addess object data 
+    _outputDiv.innerText = `num2: ${num2}, name: ${obj2.name}, price: ${obj2.price}`;
+    
+}
+
+
+
