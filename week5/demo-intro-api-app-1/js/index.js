ef00ff197c981847856ec7b79956a8637fa8e448
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-21 18:58:26 +0100

    update Demo Api Intro App 1

--- week5/demo-intro-api-app-1/js/index.js
+++ week5/demo-intro-api-app-1/js/index.js
@@ -7,16 +7,19 @@ function page_load()
 
 function js_obj_ex1()
 {
-    const msg = "## js object example 1"
+    const msg = "## js object example 1" // string 
     console.log(msg)
 
-    const num1 = 5
-    const num2 = 5.1
+    const num1 = 5      //integer - non decimal 
+    const num2 = 5.1    //float - decimal point 
 
-    const obj1 = {} //empty object data tyep - also a JSON object 
-    
-    const obj2 = {name:"name1", price:1.0} //object data tyep - also a JSON object 
+    const obj1 = {} //empty object data type - also a JSON object 
+    //dynamic - complex data types - can contain any valid datatype and collections, array/list,set,map etc
 
+    const obj2 = {name:"name1", price:1.0} //object data tyep - also a JSON object - key/value 
+
+    //display object as string 
+    //convert json object to string 
     console.log(`obj2: ${obj2}`)
 
     //const _outputDiv = document.getElementById("output")
