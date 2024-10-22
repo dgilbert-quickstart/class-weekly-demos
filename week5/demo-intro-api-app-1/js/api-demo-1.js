42234777b60c75fdb9f578fda2218b15ea642483
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-22 22:53:27 +0100

    update Api Demo 1

--- week5/demo-intro-api-app-1/js/api-demo-1.js
+++ week5/demo-intro-api-app-1/js/api-demo-1.js
@@ -114,6 +114,58 @@ const btn_jquery_ex1 = document.getElementById("btn-jquery-ex1").addEventListene
 });
 
 
+//const btn_fetch_ex1 = document.querySelector(".btn-fetch-ex1")
+//const btn_fetch_ex1 = document.querySelector("#btn-fetch-ex1")
+const btn_fetch_ex1 = document.getElementById("btn-fetch-ex1").addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    if(outputDev == null | outputDev == undefined){       
+        const msg = "## api demo 1 - output div not found"
+        console.log(msg) 
+        return false;
+    }
+
+    const msg = "## btn-fetch-ex1";
+
+    outputDev.innerText = msg;
+
+    const _api_url_user_endpoint = "https://jsonplaceholder.org/users?id=1";
+    const _api_url_comments_endpoint = "https://jsonplaceholder.org/comments?id=1";
+    const _api_url_posts_endpoint = "https://jsonplaceholder.org/posts?id=1";
+    const _url_giphy_endpoint = "";
+    const _url_data_file_enpoint = "./data/demo1.json";
+
+    //fetch - then - promise - non-blocking - javascript call then when data is available 
+    //another way of implementing promise = async/await 
+
+    fetch(_api_url_posts_endpoint)
+    .then(response => {
+      if (!response.ok) {
+        //throw - generate or a error and descrption 
+        throw new Error('Network response was not ok');
+      }
+      //-- convert data from text to json 
+      return response.json();
+    })
+    .then(data => {
+
+        console.log("#### post data ###")
+        console.log(data);
+
+        //-- display first row of post data 
+        outputDev.innerText = JSON.stringify(data[0]);
+
+    })
+    .catch(error => {
+      console.error('T## here was a problem with the fetch operation:', error);
+      outputDev.innerText = error;
+    });
+
+    outputDev.innerText = "..continue fteching data...";
+
+});
+
 
 
 
