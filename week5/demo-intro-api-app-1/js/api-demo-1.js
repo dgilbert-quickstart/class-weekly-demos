cea0e0f09dbd307e38a7b9ad653e72edc0eb8cdd
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-22 17:49:19 +0100

    update Api Demo 1

--- week5/demo-intro-api-app-1/js/api-demo-1.js
+++ week5/demo-intro-api-app-1/js/api-demo-1.js
@@ -21,8 +21,35 @@ function page_load()
 
 
 //## using DOM to bind button events 
-//-- js callback methods
-//-- js ananymous method 
-//-- exception handling 
+//- js callback methods - es1-4+ - called by javscript or within code 
+//- js ananymous method  - es5+
+//- js arrow functions  - es5+
+//- promises - es5+
+//- async/await  - es5+
+//- exception handling - es1-4+
+
+//const btnajaxex1 = document.querySelector(",.btn-ajax-ex1")
+//const btnajaxex1 = document.querySelector("#btn-ajax-ex1")
+const btnajaxex1 = document.getElementById("btn-ajax-ex1").addEventListener("click", /*callback function*/ function(event){
+
+    //-- can cancel or continue button click 
+    //-- use default
+    event.preventDefault();
+
+    if(outputDev == null | outputDev == undefined){       
+        const msg = "## api demo 1 - output div not found"
+        console.log(msg) 
+        return false;
+    }
+
+    const msg = "## btn-ajax-ex1";
+
+    outputDev.innerText = msg;
+
+});
+
+
+
+
 
 
