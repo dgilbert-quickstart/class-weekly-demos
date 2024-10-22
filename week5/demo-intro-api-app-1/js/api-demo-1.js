6e15422b595143365e7146777756249073556207
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-22 17:31:18 +0100

    update Api Demo 1

--- week5/demo-intro-api-app-1/js/api-demo-1.js
+++ week5/demo-intro-api-app-1/js/api-demo-1.js
@@ -1,7 +1,27 @@
+//page level global variable 
+
+//access page element after page is loaded 
+const outputDev = document.getElementById("output");
+//const outputDev = document.querySelector("#output");
+//const outputDev = document.querySelector(".output");
+
 function page_load()
 {
     const msg = "## api demo 1 - page load"
     console.log(msg)
+
+    if(outputDev == null | outputDev == undefined){       
+        const msg = "## api demo 1 - output div not found"
+        console.log(msg) 
+        return false;
+    }
+
+    outputDev.innerText = msg;
 }
 
+
+
+//## using DOM to bind button events 
+//-- js callback methods
+//-- js ananymous method 
 
