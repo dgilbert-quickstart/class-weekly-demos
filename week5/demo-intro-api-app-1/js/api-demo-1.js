ef2a7a1e39534795c2b9cd443be5784873d500c3
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-22 22:09:35 +0100

    update Api Demo 1

--- week5/demo-intro-api-app-1/js/api-demo-1.js
+++ week5/demo-intro-api-app-1/js/api-demo-1.js
@@ -18,6 +18,7 @@ xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
         //data is avaliable 
         //JSON.parse - convert text json data to json object 
+        console.log("## xjax/xhr - Users Data: " )
         console.log(JSON.parse(xhr.responseText));
         outputDev.innerText = xhr.responseText;
     }
@@ -46,8 +47,8 @@ function page_load()
 //- async/await  - es5+
 //- exception handling - es1-4+
 
-//const btn_ajax_xhr_ex1 = document.querySelector(".btn-ajax-xhr-ex1"")
-//const btn_ajax_xhr_ex1 = document.querySelector("#btn-ajax-xhr-ex1"")
+//const btn_ajax_xhr_ex1 = document.querySelector(".btn-ajax-xhr-ex1")
+//const btn_ajax_xhr_ex1 = document.querySelector("#btn-ajax-xhr-ex1")
 const btn_ajax_xhr_ex1 = document.getElementById("btn-ajax-xhr-ex1").addEventListener("click", function(event){
 
     //-- can cancel or continue button click 
@@ -74,6 +75,42 @@ const btn_ajax_xhr_ex1 = document.getElementById("btn-ajax-xhr-ex1").addEventLis
     //can me call asyncrynous - non blocking 
     xhr.open('GET', _api_url_user_endpoint, true);
     xhr.send();     
+
+});
+
+//const btn_jquery_ex1 = document.querySelector(".btn-jquery-ex1")
+//const btn_jquery_ex1 = document.querySelector("#btn-jquery-ex1")
+const btn_jquery_ex1 = document.getElementById("btn-jquery-ex1").addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    if(outputDev == null | outputDev == undefined){       
+        const msg = "## api demo 1 - output div not found"
+        console.log(msg) 
+        return false;
+    }
+
+    const msg = "## btn-jquery-ex1";
+
+    outputDev.innerText = msg;
+
+    const _api_url_user_endpoint = "https://jsonplaceholder.org/users?id=1";
+    const _api_url_comments_endpoint = "https://jsonplaceholder.org/comments?id=1";
+    const _api_url_posts_endpoint = "https://jsonplaceholder.org/posts?id=1";
+    const _url_giphy_endpoint = "";
+    const _url_data_file_enpoint = "./data/demo1.json";
+
+    //jquery example javascript plugin reference 
+    //src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" defer
+
+    //$.getJSON(_api_url_comments_endpoint, function(data, status)
+    $.get(_api_url_comments_endpoint, function(data, status)
+    {
+        console.log("Jquery - Comments Data: " + "\nStatus: " + status)
+        console.log(data);
+        outputDev.innerText = JSON.stringify(data);
+
+    });
 
 });
 
