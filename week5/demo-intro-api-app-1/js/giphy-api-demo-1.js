4e02880651124e516f773586d07a15d5b4941463
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-23 21:34:03 +0100

    update giphy api demo 1 - try catch error handling

--- week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
+++ week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
@@ -5,16 +5,34 @@ const outputDiv = document.getElementById("output");
 
 async function page_load()
 {
-    const msg = "## giphy api demo 1 - page load"
-    console.log(msg)
+    
+    //
+    //-- try -- catch error handler 
+    //- when an error occurs inside the try code 
+    //- code continues to the catch block
+    //
 
-    if(outputDiv == null | outputDiv == undefined){       
-        const msg = "## giphy api demo 1 - output div not found"
-        console.log(msg) 
-        return false;
+    try {
+                    
+        const msg = "## giphy api demo 1 - page load"
+        console.log(msg)
+
+        if(outputDiv == null | outputDiv == undefined){       
+            const msg = "## giphy api demo 1 - output div not found"
+            console.log(msg) 
+            return false;
+        }
+    
+        outputDiv.innerText = msg;
+
+    } catch (error) {
+
+        console.log("## gihpy demo appp 1 - error ")
+        console.log(error)
+
+        msg = "## gihpy demo appp 1 - error ";
+        outputDiv.innerText = msg;
     }
- 
-    outputDiv.innerText = msg;
 }
 
 
