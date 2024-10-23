abe49c5c40667b13f5c93787b252c6c5c5a764d8
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-23 22:10:41 +0100

    update giphy api demo 1 - call giphy api

--- week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
+++ week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
@@ -36,3 +36,31 @@ async function page_load()
 }
 
 
+document.getElementById("btn-giphy-api-ex1").addEventListener("click",async =>{
+
+    try {
+                    
+        const msg = "## btn-giphy-api-ex1"
+        console.log(msg)
+
+        if(outputDiv == null | outputDiv == undefined){       
+            const msg = "## giphy api demo 1 - output div not found"
+            console.log(msg) 
+            return false;
+        }
+    
+        outputDiv.innerText = msg;
+
+    } catch (error) {
+
+        console.log("## btn-giphy-api-ex1 - error ")
+        console.log(error)
+
+        msg = "## btn-giphy-api-ex1 - error ";
+        outputDiv.innerText = msg;
+    }
+
+})
+
+
+
