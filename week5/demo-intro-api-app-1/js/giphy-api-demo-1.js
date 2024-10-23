66a7080bdb7525098a5967b28cf3dbab01e4b8a6
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-23 21:18:39 +0100

    giphy api demo 1

---
+++ week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
@@ -0,0 +1,20 @@
+
+//const outputDiv = document.querySelector(".output");
+//const outputDiv = document.querySelector("#output");
+const outputDiv = document.getElementById("output");
+
+async function page_load()
+{
+    const msg = "## giphy api demo 1 - page load"
+    console.log(msg)
+
+    if(outputDiv == null | outputDiv == undefined){       
+        const msg = "## giphy api demo 1 - output div not found"
+        console.log(msg) 
+        return false;
+    }
+
+    outputDiv.innerText = msg;
+}
+
+
