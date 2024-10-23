2e0866c759de2bd48edac60590953b386c34360d
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-23 22:43:28 +0100

    update giphy api demo 1 - api search

--- week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
+++ week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
@@ -63,7 +63,32 @@ document.getElementById("btn-giphy-api-ex1").addEventListener("click",async =>{
         const _giphy_url_1 = `https://api.giphy.com/v1/gifs/search?api_key=${_giphy_apikey}&q=${_search_text}`;
         const _giphy_url_2 = `https://api.giphy.com/v1/gifs/search?q=${_search_text}&api_key=${_giphy_apikey}`;
 
+        const _url_giphy_demo_data_url = "./data/giphy.json";
+
         //## can add other parameters ie: search limit, etc..
+            
+        //fetch(_giphy_url_1)
+        fetch(_url_giphy_demo_data_url)
+        .then(response => {
+            if (!response.ok) {
+                //throw/raise - generate or a error and descrption 
+                throw new Error('Network response was not ok');
+            }
+            //-- convert data from text to json 
+            return response.json();
+        })
+        .then(data => {
+
+            console.log("#### fetch giphy data result ###")
+            console.log(data);
+
+            outputDev.innerText = "## fetch giphy data result ##";
+
+        })
+        .catch(error => {
+            console.error('T## here was a problem with the fetch operation:', error);
+            outputDev.innerText = error;
+        });
 
     } catch (error) {
 
