713a479cda1907ca3d0a5daddec9209229e0035b
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-24 18:20:38 +0100

    update giphy api app 1, btt-search

--- week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
+++ week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
@@ -121,9 +121,43 @@ document.getElementById("btn-search").addEventListener("click",async =>{
             return false;
         }
 
-        msg = `* searching for: ${_txtsearch.value}`
-        outputDiv.innerText = msg;
+        //msg = `* searching for: ${_txtsearch.value}`
+        //outputDiv.innerText = msg;
 
+        const _url_giphy_demo_data_url = "./data/giphy2.json";
+
+        const _giphy_apikey = "demo-example-api-key-Ab2422Ab43"
+        const _search_text = _txtsearch.value;
+        const _giphy_url_1 = `https://api.giphy.com/v1/gifs/search?api_key=${_giphy_apikey}&q=${_search_text}`;
+        const _giphy_url_2 = `https://api.giphy.com/v1/gifs/search?q=${_search_text}&api_key=${_giphy_apikey}`;
+
+        //## can add other parameters ie: search limit, etc..
+        
+        //single line if statement (logical-expression) ? true : false;
+        const _url = (_giphy_apikey == "demo-example-api-key-Ab2422Ab43")?_url_giphy_demo_data_url:_giphy_url_1;
+
+        fetch(_url)
+        .then(response => {  
+            if (!response.ok) {
+                //throw/raise - generate or a error and descrption 
+                throw new Error('Network response was not ok');
+            }
+            //-- convert data from text to json 
+            return response.json();
+        })
+        .then(data => { 
+
+            console.log("#### fetch giphy data result ###")
+            console.log(data);
+
+            //## data.images.original.url 
+            outputDiv.innerText = "## fetch giphy data result ##";
+
+        })
+        .catch(error => {
+            console.error('T## here was a problem with the fetch operation:', error);
+            outputDiv.innerText = error;
+        });
 
 
     } catch (error) {
