53dc132235facb7a39a8a5636611adddcf36e40e
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-10-24 17:02:54 +0100

    update giphy api app 1, url request

--- week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
+++ week5/demo-intro-api-app-1/js/giphy-api-demo-1.js
@@ -51,13 +51,7 @@ document.getElementById("btn-giphy-api-ex1").addEventListener("click",async =>{
     
         outputDiv.innerText = msg;
 
-        //http://developers.giphy.com/docs/api
-        //https://developers.giphy.com/docs/api/endpoint#search
-        //http://developers.giphy.com/docs/explorer
-
-        const _giphy_url_demo_1 = "https://api.giphy.com/v1/gifs/search?api_key=&q=";
-        const _giphy_url_demo_2 = "https://api.giphy.com/v1/gifs/search?q=&api_key=";
-        const _url_giphy_demo_data_url = "./data/giphy1.json";
+        const _url_giphy_demo_data_url = "./data/giphy2.json";
 
         const _giphy_apikey = "demo-example-api-key-Ab2422Ab43"
         const _search_text = "cars"; //retieve search text from input field
@@ -65,9 +59,11 @@ document.getElementById("btn-giphy-api-ex1").addEventListener("click",async =>{
         const _giphy_url_2 = `https://api.giphy.com/v1/gifs/search?q=${_search_text}&api_key=${_giphy_apikey}`;
 
         //## can add other parameters ie: search limit, etc..
-            
-        fetch(_giphy_url_1)
-        //fetch(_url_giphy_demo_data_url)
+        
+        //single line if statement (logical-expression) ? true : false;
+        const _url = (_giphy_apikey == "demo-example-api-key-Ab2422Ab43")?_url_giphy_demo_data_url:_giphy_url_1;
+
+        fetch(_url)
         .then(response => {  
             if (!response.ok) {
                 //throw/raise - generate or a error and descrption 
