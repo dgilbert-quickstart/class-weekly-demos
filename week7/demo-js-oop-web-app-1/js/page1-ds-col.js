0b831c74b4e2c8a85b6e118ccb4ce88c01362468
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-04 19:14:55 +0000

    update Page1 Ds/Collections

--- week7/demo-js-oop-web-app-1/js/page1-ds-col.js
+++ week7/demo-js-oop-web-app-1/js/page1-ds-col.js
@@ -5,13 +5,41 @@ const outputDiv = document.getElementById("output");
 document.getElementById("btn-ds-coll-ex1").addEventListener("click", (event)=>{
 
     let msg = "";
+    
+    //define/creating array/collection 
+    //-1 array, object , mixed/nested -array & object
+    //-2 looping throught 
+    //-3 filtering, finding 
+    //-4 add, update, delete
+    //- array index - 0..1, array/collection length
+
+    //- integer array - data in-memory 
+    const items_list_1 = [1,2,3,4,5,6];
 
     try {
         
         msg =  "# btn-ds-coll-ex1"
         outputDiv.innerText = msg
-
         console.log(msg)
+
+        //- display all array items as string 
+        //msg = JSON.stringify(items_list_1);
+        msg = items_list_1.toString() + "<p>";
+
+        //-length of array         
+        msg += `array length: ${items_list_1.length} <p>`
+        
+        // access array by index 
+        msg += `array index 0: ${items_list_1[0]} <p>`
+        
+        // access array by index 
+        msg += `array index 5: ${items_list_1[5]} <p>`
+        
+        //error - index 6 = out of bound or out of array length 
+        //msg += `array index 5: ${items_list_1[6]} <p>`
+
+        outputDiv.innerHTML = msg;
+
 
     } catch (error) {
 
