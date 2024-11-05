9abeaa71ec789ea53531317629f95c5279db09c0
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-05 18:04:31 +0000

    update page2-ds-col-filter

--- week7/demo-js-oop-web-app-1/js/page2-ds-col-filter.js
+++ week7/demo-js-oop-web-app-1/js/page2-ds-col-filter.js
@@ -39,3 +39,54 @@ document.getElementById("btn-ds-coll-filter-ex1").addEventListener("click", (eve
         console.log(error)
     }
 });
+
+
+
+document.getElementById("btn-ds-coll-filter-ex2").addEventListener("click", (event)=>{
+
+    let msg = "";
+    
+    //define/creating array/collection 
+    //-3 filtering, finding 
+
+    //- integer array - data in-memory 
+    const items_list_1 = [1,2,3,4,3,5,6,7,5];
+
+    try {
+        
+        msg =  "# btn-ds-coll-filter-ex2"
+        outputDiv.innerText = msg
+        console.log(msg)
+
+        const _search1 = 5;
+        const _search2 = 10;
+
+        //-- finds first item matching the if statement 
+
+        const _found1 = items_list_1.find((item,index)=>{
+            //index
+            return item == _search1
+        });
+
+        const _found2 = items_list_1.find((item,index)=>{
+            //index
+            return item == _search2
+        });
+
+        msg = `${items_list_1.toString()} <p>`
+        msg += `search1: ${_search1} found1: ${_found1} - search2: ${_search2} found2: ${_found2}`
+
+        outputDiv.innerHTML = msg;
+
+
+    } catch (error) {
+
+        msg =  "# btn-ds-coll-filter-ex2: error"
+        outputDiv.innerText = msg
+
+        console.log(msg)
+        console.log(error)
+    }
+});
+
+
