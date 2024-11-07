60d8460566557e5eaa287b684831da5fb1182997
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-07 18:47:58 +0000

    update page6-js-oop-es4

--- week7/demo-js-oop-web-app-1/js/page6-js-oop-es4.js
+++ week7/demo-js-oop-web-app-1/js/page6-js-oop-es4.js
@@ -114,7 +114,59 @@ document.getElementById("btn-es4-oop-ex2").addEventListener("click", (event)=>{
 
     } catch (error) {
 
-        msg =  "# btn-es4-oop-ex1: error"
+        msg =  "# btn-es4-oop-ex2: error"
+        outputDiv.innerText = msg
+
+        console.log(msg)
+        console.log(error)
+    }
+});
+
+
+document.getElementById("btn-es4-oop-ex3").addEventListener("click", (event)=>{
+
+    let msg = "";
+    
+    try {
+        
+        msg =  "# btn-es4-oop-ex3"
+        outputDiv.innerText = msg
+        console.log(msg)
+        
+        //class/template definition
+        //function initilization / constructor 
+        //-function with paramerer
+        //- constructor - used to initialize call data when class instance is created in memory 
+        //-- used in react:        
+        function contact(name, email) {
+            this.name = name
+            this.email = email
+            this.getInfo = function()
+            {
+                //this = get data for the current instace/copy of object in memory/space 
+                //return "## name: " + name + ", " + this.email
+                return "## name: " + this.name + ", " + this.email
+            }
+        }
+
+        //# re-use calss - create a intsance or creat clas wirth data in memory 
+        const person1 = new contact("sam","sam@gmail.com")
+
+        const person2 = new contact("ann","ann@yahoo.com")
+    
+        //person1.phone = "1268-26-267"
+        person1["phone"] = "1268-26-267"
+
+        msg = `## preson1 : ${person1.getInfo()} , phone: ${person1.phone}<p>`
+        msg += `## preson2 : ${person2.getInfo()} , phone: ${person2.phone}<p>`
+        msg += `## preson1 : ${person1.getInfo()} , phone: ${person1.phone}<p>`
+
+        outputDiv.innerHTML = msg;
+
+
+    } catch (error) {
+
+        msg =  "# btn-es4-oop-ex3: error"
         outputDiv.innerText = msg
 
         console.log(msg)
