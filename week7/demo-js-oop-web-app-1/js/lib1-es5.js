5fc554aa205669b7694b5b195ad8aea9014ce387
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-08 19:01:54 +0000

    update page8-lib-demo

---
+++ week7/demo-js-oop-web-app-1/js/lib1-es5.js
@@ -0,0 +1,15 @@
+//lib1-es5.js
+
+//es5+ modules 
+//any valid es5+ code 
+//export, import - variables, class, functions , other 
+
+//export function lib1_es5_func1()
+export const lib1_es5_func1 = () =>
+{
+    const msg = `## lib1-es5::func1`
+    console.log(msg)
+
+    return msg;
+}
+
--- week7/demo-js-oop-web-app-1/js/page8-lib-demo.js
+++ week7/demo-js-oop-web-app-1/js/page8-lib-demo.js
@@ -1,5 +1,13 @@
 //page8-lib-demo.js
 
+//import es5+ modules
+//script src="js/page8-lib-demo.js" type="module" 
+//- js file can now import any valid es5+ exported code 
+//- type=module can use es4 and es5+ code 
+
+//object destructuring - retrieve function name from exported file 
+import {lib1_es5_func1} from "./lib1-es5.js"
+
 const outputDiv = document.getElementById("output");
 
 document.getElementById("btn-es4-lib-bundle-ex1").addEventListener("click", (event)=>{
@@ -15,6 +23,38 @@ document.getElementById("btn-es4-lib-bundle-ex1").addEventListener("click", (eve
         msg = lib1_es4_func1() + "<p>";
         msg += bundle1_es4_func1() + "<p>";
         msg += bundle1_es4_func2()+ "<p>";
+
+        outputDiv.innerHTML = msg;
+
+    } catch (error) {
+
+        msg =  "# btn-es4-lib-bundle-ex1: error"
+        outputDiv.innerText = msg
+
+        console.log(msg)
+        console.log(error)
+    }
+
+});
+
+
+document.getElementById("btn-es5-lib-bundle-ex1").addEventListener("click", (event)=>{
+
+    let msg = "";
+    
+    try {
+        
+        msg =  "# btn-es5-lib-bundle-ex1"
+        outputDiv.innerText = msg
+        console.log(msg)
+        
+        //--- es4 -- include 
+        msg = lib1_es4_func1() + "<p>";
+        msg += bundle1_es4_func1() + "<p>";
+        msg += bundle1_es4_func2()+ "<p>";
+
+        //-- es5 module include 
+        msg += lib1_es5_func1() + "<p>";
 
         outputDiv.innerHTML = msg;
 
