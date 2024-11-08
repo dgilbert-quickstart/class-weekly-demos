1b781b5425f3b65ea29aacb09c867a3726786aa4
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-08 18:02:40 +0000

    update page8-lib-demo

---
+++ week7/demo-js-oop-web-app-1/js/bundle1-es4.js
@@ -0,0 +1,22 @@
+//bundle1-es4.js
+
+//-- benerate - compbine code 1 or more more js files into 1 or more bundle files 
+//-- also: minifications, minifying, code splitting 
+
+function bundle1_es4_func1()
+{
+    const msg = "## bindle1-es4::func1"
+    console.log(msg)
+
+    return msg;
+}
+
+
+function bundle1_es4_func2()
+{
+    const msg = "## bindle1-es4::func2"
+    console.log(msg)
+
+    return msg;
+}
+
--- week7/demo-js-oop-web-app-1/js/page8-lib-demo.js
+++ week7/demo-js-oop-web-app-1/js/page8-lib-demo.js
@@ -2,23 +2,25 @@
 
 const outputDiv = document.getElementById("output");
 
-document.getElementById("btn-es4-lib-ex1").addEventListener("click", (event)=>{
+document.getElementById("btn-es4-lib-bundle-ex1").addEventListener("click", (event)=>{
 
     let msg = "";
     
     try {
         
-        msg =  "# btn-es4-lib-ex1"
+        msg =  "# btn-es4-lib-bundle-ex1"
         outputDiv.innerText = msg
         console.log(msg)
         
-        msg = lib1_es4_func1();
+        msg = lib1_es4_func1() + "<p>";
+        msg += bundle1_es4_func1() + "<p>";
+        msg += bundle1_es4_func2()+ "<p>";
 
         outputDiv.innerHTML = msg;
 
     } catch (error) {
 
-        msg =  "# btn-es4-lib-ex1: error"
+        msg =  "# btn-es4-lib-bundle-ex1: error"
         outputDiv.innerText = msg
 
         console.log(msg)
