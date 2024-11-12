816b189480b35b6e7645fb978d2b874fe5aa2772
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-12 20:54:11 +0000

    update react demo app 1

--- week8/react-development/react-demo-app-1/src/App.js
+++ week8/react-development/react-demo-app-1/src/App.js
@@ -1,10 +1,22 @@
+
+//react componeent/webpage or html snipit 
+//ie: app.html , page1.html - exctpt -- html-head->body
+//jsx - modified version of html ie: <div class = <div className
+//
+//react component/page design = root element 
+// example: <div>...</div>
+// erorr if 2 root elements ie: <div>...</div><div>...</div>
+// or react root element <></>
+
 function App() {
   return (
-    <div className="App">
-        <p>
-          React demo app 1.0.0
-        </p>
-    </div>
+    <>
+      <div>
+          <p>
+            React demo app 1.0.0
+          </p>
+      </div>
+    </>
   );
 }
 
