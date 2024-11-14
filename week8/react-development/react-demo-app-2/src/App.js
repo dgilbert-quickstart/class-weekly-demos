00754fa3db5dbac78ccd0cd6cd2e9765983bce99
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-14 20:59:31 +0000

    update react demo website 2

--- week8/react-development/react-demo-app-2/src/App.js
+++ week8/react-development/react-demo-app-2/src/App.js
@@ -1,10 +1,9 @@
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <div>
-        <p>React demo app 2.0.0</p>
-      </div>
+      <Home/>
     </>
   );
 }
---
+++ week8/react-development/react-demo-app-2/src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <>
+          <h1>React demo app 2.0.0</h1>        
+      </>
+    );
+}
+  
+export default Home;
+
