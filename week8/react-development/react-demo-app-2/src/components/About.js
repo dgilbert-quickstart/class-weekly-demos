00ef77fdcf150181e55b891b368815b046262a48
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-14 22:25:28 +0000

    update react demo website 2, brwoser router dom, link

--- week8/react-development/react-demo-app-2/src/components/About.js
+++ week8/react-development/react-demo-app-2/src/components/About.js
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 function About() {
     return (
       <>
+          <nav>
+            <Link to="/">Home</Link> {" | "}
+            <Link to="/about">About</Link>
+          </nav>
+          <p></p>
           <p>About page/component</p>
       </>
     );
--- week8/react-development/react-demo-app-2/src/components/Home.js
+++ week8/react-development/react-demo-app-2/src/components/Home.js
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-      <>
+      <> 
+          <nav>
+            <Link to="/">Home</Link> {" | "}
+            <Link to="/about">About</Link>
+          </nav>
+          <p></p>
           <h1>React demo app 2.0.0</h1>        
       </>
     );
