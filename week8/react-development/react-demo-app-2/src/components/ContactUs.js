ce2cad28792dbb3ddf86ec08ceb9f2714cad9fbb
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-15 17:32:41 +0000

    update react demo website 2, brwoser router dom, link, NavBar

--- week8/react-development/react-demo-app-2/src/components/ContactUs.js
+++ week8/react-development/react-demo-app-2/src/components/ContactUs.js
@@ -2,7 +2,20 @@ function ContactUs() {
     return (
       <>
           <p></p>
-          <p>ContactUs page/component</p>
+          <p>ContactUs page/component 1.0.1</p>
+          <p>
+            <div id="output" className="output"></div>
+          </p>
+          <div>
+            <form>
+              <label>* Name: </label><input maxLength={20}></input><br></br>
+              <label>* Email: </label><input  maxLength={30}></input><br></br>
+              <label> Phone: </label><input  maxLength={15}></input><br></br>
+              <label>* Comment: </label><br></br>
+              <textarea rows={5} cols={30}></textarea><p></p>
+              <button>Submit</button>
+            </form>
+          </div>
       </>
     );
 }
