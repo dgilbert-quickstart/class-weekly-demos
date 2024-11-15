b660bc4a6776177f0768c49715e72ff6417eb395
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-15 18:15:16 +0000

    update react demo website 2, contact form, button click

--- week8/react-development/react-demo-app-2/src/components/ContactUs.js
+++ week8/react-development/react-demo-app-2/src/components/ContactUs.js
@@ -1,8 +1,30 @@
 function ContactUs() {
+
+    //react includes event object as first parameter 
+    function btnHandleFormValidation(event)
+    {
+      event.preventDefault();
+
+      try {
+        
+        const _msg = "## btnHandleFormValidation"
+        alert(_msg)
+        console.log(_msg)
+
+      } catch (error) {        
+        const _msg = "## btnHandleFormValidation::Error"
+        alert(_msg)
+        console.log(_msg)
+        console.log(error)
+      
+      }
+
+    }
+
     return (
       <>
           <p></p>
-          <p>ContactUs page/component 1.0.1</p>
+          <p>ContactUs page/component 1.0.2</p>
           <p>
             <div id="output" className="output"></div>
           </p>
@@ -13,7 +35,7 @@ function ContactUs() {
               <label> Phone: </label><input  maxLength={15}></input><br></br>
               <label>* Comment: </label><br></br>
               <textarea rows={5} cols={30}></textarea><p></p>
-              <button>Submit</button>
+              <button onClick={btnHandleFormValidation}>Submit</button>
             </form>
           </div>
       </>
