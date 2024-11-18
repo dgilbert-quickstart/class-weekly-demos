a27fd4539f36a4c2ba8ea6ca11b65f41bd06960e
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-11-18 16:55:21 +0000

    react demo app 2 - constactus2

--- week8/react-development/react-demo-app-2/src/components/ContactUs2.js
+++ week8/react-development/react-demo-app-2/src/components/ContactUs2.js
@@ -3,6 +3,21 @@ function ContactUs2() {
       <>
           <p></p>
           <p>Contact Us 2 page/component</p>
+          <p></p>
+          <div className="output"></div>
+          <p></p>
+          <div>
+            <form>
+              <label>* Name: </label><input type="text" maxLength={20}></input><br></br>
+              <label>* Email: </label><input  type="text"  maxLength={30}></input><br></br>
+              <label> Phone: </label><input type="text"  maxLength={15}></input><br></br>
+              <label>* Comment: </label><br></br>
+              <textarea  rows={5} cols={30}></textarea><p></p>
+              <button>Submit</button>
+              {"   "}
+              <button>reset</button>
+            </form>
+          </div>
       </>
     );
 }
