5919a3426b0e508d7401fb8aadc0746486efb4ad
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-02 19:06:06 +0000

    create node express backend demo app 1

---
+++ week11/back-end-development/node-express-api-demo-1/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express"); //import express library 
+const app = express(); //create a new express instance in memory 
+
+//- node middleware 
+
+const SERVER_PORT = 8080; //localhost range - 3000 - 9999
+
+//- GET endpoint - url endpoint or route - ie: json placeholder, githpy api 
+app.get("/", (req,res) => {
+
+    const _msg = "# Demo node express api 1.0.0";
+    console.log(_msg)
+    res.send(_msg)
+
+});
+
+//-other - POST, DELETE, PUT
+
+//-start node exporess web server - ie: live server
+app.listen(SERVER_PORT, ()=>{
+    let _msg = "node express websever running at port: " + SERVER_PORT;
+    console.log(_msg);
+})
+
+
+//node src/index.js 
