9fb17f385ed983e7cdb38fe0e45b0846942a5364
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-02 22:25:26 +0000

    update web front-end api demo 1

--- week11/front-end-development/web-front-end-api-demo-1/src/js/index.js
+++ week11/front-end-development/web-front-end-api-demo-1/src/js/index.js
@@ -1,2 +1,22 @@
 //index.js 
 
+const outputDiv = document.getElementById("output")
+
+document.getElementById("btn-call-api-demo-1").addEventListener("click", (e)=>{
+
+    let _msg = "# btn-call-api-demo-1"
+
+    try {
+        
+        console.log(_msg)
+        outputDiv.innerText = _msg;
+
+    } catch (error) {
+        _msg = "#btn-call-api-demo-1::error"
+        
+        outputDiv.innerText = _msg;
+
+        console.log(_msg)
+        console.log(error)
+    }
+})
