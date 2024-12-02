bf659d82e9ce52e83393144c2a7344ccfe769fe4
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-12-02 22:34:53 +0000

    update web front-end api demo 1

--- week11/front-end-development/web-front-end-api-demo-1/src/js/index.js
+++ week11/front-end-development/web-front-end-api-demo-1/src/js/index.js
@@ -8,8 +8,33 @@ document.getElementById("btn-call-api-demo-1").addEventListener("click", (e)=>{
 
     try {
         
-        console.log(_msg)
-        outputDiv.innerText = _msg;
+        //console.log(_msg)
+        //outputDiv.innerText = _msg;
+
+        const _url = "http://localhost:8080/about";
+
+        fetch(_url)
+        .then(res=>res.text())
+        .then(data =>  outputDiv.innerText = data)
+        .catch(err => {
+          _msg = "# fetch api error "  
+          outputDiv.innerText = _msg
+          console.log(_msg)
+          console.log(err)
+        });    
+
+        //### localhost web access error ### //
+        //### access origin error - same local host access error ### //
+
+        /*
+            Access to fetch at 'http://localhost:8080/about' 
+            from origin 'http://127.0.0.1:5500' has been blocked by 
+            CORS policy: No 'Access-Control-Allow-Origin' 
+            header is present on the requested resource. 
+            If an opaque response serves your needs, 
+            set the request's mode to 'no-cors' to 
+            fetch the resource with CORS disabled.
+        */
 
     } catch (error) {
         _msg = "#btn-call-api-demo-1::error"
